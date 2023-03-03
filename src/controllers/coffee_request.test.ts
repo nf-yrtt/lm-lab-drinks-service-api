aa9da7efaa1dd642f1d64949ccb8c1df789925d8
Nigel Faria <nigel.faria@gmail.com>
2023-03-03 10:41:33 +0000

    fix: rename controller files to snakecase

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import { app } from '../app';
 
-describe('Test Coffee Lover API endpoint request', () => {
+describe('Test Coffee  API endpoint request', () => {
     test('Get should return correct message', async () => {
         const res = await request(app).get('/coffeelover');
         expect (res.statusCode).toEqual(200);
